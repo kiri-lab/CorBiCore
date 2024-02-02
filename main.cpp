74694c75f53f952816e051872dbb1f8323371a2a
kirimine170 <kirimine170@gmail.com>
2024-02-02 20:49:24 +0900

    ✏️ update main.cpp
    とりあえずダミーをCO

--- main.cpp
+++ main.cpp
@@ -8,16 +8,16 @@
 // とりま勉強がてら
 int main()
 {
-    std::random_device rd;                           // 真乱数。ハードウェアのノイズを使ってるらしい
-    std::mt19937 mt(rd());                           // 疑似乱数のシードを真乱数にしてるっぽい
-    std::uniform_int_distribution<int> dist(0, 300); // 0~300の範囲指定。あんまし分かってないので調べたい
+    // std::random_device rd;                           // 真乱数。ハードウェアのノイズを使ってるらしい
+    // std::mt19937 mt(rd());                           // 疑似乱数のシードを真乱数にしてるっぽい
+    // std::uniform_int_distribution<int> dist(0, 300); // 0~300の範囲指定。あんまし分かってないので調べたい
 
-    for (;;)
-    {
-        std::cout << dist(mt) << std::endl;
+    // for (;;)
+    // {
+    //     std::cout << dist(mt) << std::endl;
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_TIME));
-    }
+    //     std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_TIME));
+    // }
 
-    return 0;
+    // return 0;
 }
